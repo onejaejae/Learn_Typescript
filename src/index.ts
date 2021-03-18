26bf86ccc87e61988c68b9d39ba61048d9425c1c
Cho Won Jae <yoteamo7@naver.com>
2021-03-18 22:58:48 +0900

    Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-const name = "Nicolas",
-    age = 24,
-    gender = "male"
-
-const sayHi = (name:string, age:number, gender:string):string => {
-   return (`Hello ${name}, You are ${age}, You are a ${gender}!`);
+class Block {
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data:string;
+    public timestamp:number;
+    
+    constructor( index:number, hash:string, previousHash:string, data:string, timestamp:number){
+        this.index=index;
+        this.hash=hash;
+        this.previousHash=previousHash;
+        this.data=data;
+        this.timestamp=timestamp;
+    }
 }
 
-console.log(sayHi(name, age, gender));
+const genesisBlock: Block = new Block(0, "2020202020","","Hello",123456);
+
+let blockchain : [Block] = [genesisBlock]
+
+console.log(blockchain)
 
 export {};
